5a15db4d7540cc92cd3df73918f73c30c22f9d5f
jaeyoung <choijy5847@naver.com>
2023-11-30 23:42:26 +0900

    11/30

--- popup/request.js
+++ popup/request.js
@@ -1,8 +1,16 @@
 chrome.runtime.sendMessage({
     contentScriptQuery: "queryOpenAI",
     text: "This is a hate speech sentence."
-}, response => {
-    if (response && response.response) {
-        console.log(response.response);
-    }
-});
+}, response = await fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+        messages: [{role: 'system', content: promptString}],
+        model: 'gpt-3.5-turbo',
+    })
+}),
+
+
